fc99a515b233adc28fbc5ac3c7cd557a97079a64
Yevhen Malyi <malyje164@gmail.com>
2022-07-04 18:08:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+            /* task#1 */
+
+
+            /* for (var i = 0; i <= 135; i++) {
+                console.log(i)
+                } */
+
+
+            /* task#2 */
+
+
+            /*  for (let i = 0; i <= 135; i++) {
+                 if (i % 2 == 0) continue;
+                 console.log(i);
+             } 
+
+            /* task#3 */
+
+            /* var sum = 0;
+              var b = 0;
+              for (var i = 0; i <= 135; i++) {
+                  sum = i + (b + i);
+                  console.log(`number= ${i}`)
+                  console.log(`summ= ${sum}`)
+                  console.log(`______________________________`)
+
+              } */
+
+            /* task#4 */
+
+            /* var X = [1, 4, 2, 12];
+            for (i = 0; i < X.length; i++) {
+                console.log(X[i]);
+            } */
+
+            /* task#5 */
+
+            /* let x = [2, -3, -1];
+            console.log(Math.max(...x)) */
+
+            /* task#6 */
+
+            /*  var x = [2, 1, 3];
+             var sum = 0;
+             for (var i = 0; i < x.length; i++) { */
+
+            /*sum+=x[i]*/
+
+            /* sum = sum + x[i] 
+            }
+            var result = sum / x.length;
+            console.log(result) */
+
+            /* task#7 */
+
+            /*  function elimination(eliminate) {
+                 let result = [];
+                 for (let i = 0; i < eliminate.length; i++) {
+                     if (eliminate[i] < 0) {
+                         result.push(0);
+                     } else {
+                         result.push(eliminate[i])
+                     }
+                 }
+                 console.log(result)
+             }
+             let x = [2, -1, 4, -3]
+             elimination(x); */
+
+            /* task#8 */
+
+            /* function transforms(number) {
+                var result = [];
+                for (var i = 0; i < number.length; i++) {
+                    if (number[i] < 0) {
+                        result.push('Turing')
+                    } else {
+                        result.push(number[i])
+                    }
+                }
+                console.log(result)
+            }
+            var x = [1, -4, 0, -1];
+            transforms(x); */
